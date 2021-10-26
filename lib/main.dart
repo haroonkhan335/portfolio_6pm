3cfd2189775722ab2a216ed227fe48dd39d07f39
haroonkhan335 <m.haroon335@gmail.com>
2021-10-26 07:12:35 -0700

    simplification

--- lib/main.dart
+++ lib/main.dart
@@ -22,259 +22,216 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              SizedBox(height: 50),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(100),
-                child: Container(
-                  height: 150,
-                  width: 150,
-                  child: Image(
-                    fit: BoxFit.cover,
-                    image: AssetImage('assets/profile_image.png'),
+        child: Stack(
+          children: [
+            Container(
+              height: double.infinity,
+              width: double.infinity,
+              child: Image(
+                fit: BoxFit.cover,
+                image: AssetImage('assets/overlay.png'),
+              ),
+            ),
+            Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  colors: [
+                    Color(0xFFFBE7DE).withOpacity(0.7),
+                    Color(0xFFF8E3DA).withOpacity(0.7),
+                  ],
+                ),
+              ),
+            ),
+            Center(
+              child: Column(
+                children: [
+                  SizedBox(height: 50),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                    child: Container(
+                      height: 150,
+                      width: 150,
+                      child: Image(
+                        fit: BoxFit.cover,
+                        image: AssetImage('assets/profile_image.png'),
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Cassian',
-                style: TextStyle(
-                  fontSize: 32,
-                  color: Color(0xFFFF9881),
-                ),
-              ),
-              SizedBox(height: 10),
-              Text(
-                "Programmer",
-                style: TextStyle(
-                  fontSize: 15,
-                ),
-              ),
-              SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Icon(
-                    Icons.location_on,
-                  ),
-                  SizedBox(width: 8),
+                  SizedBox(height: 20),
                   Text(
-                    'Peshawar, Pakistan',
+                    'Cassian',
+                    style: TextStyle(
+                      fontSize: 32,
+                      color: Color(0xFFFF9881),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "Programmer",
                     style: TextStyle(
                       fontSize: 15,
                     ),
                   ),
-                ],
-              ),
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Column(
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: const [
-                      Text("23"),
-                      SizedBox(height: 5),
-                      Text("Projects"),
+                      Icon(
+                        Icons.location_on,
+                      ),
+                      SizedBox(width: 8),
+                      Text(
+                        'Peshawar, Pakistan',
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
                     ],
                   ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20),
-                    child: Text(
-                      "|",
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w300,
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        children: const [
+                          Text("23"),
+                          SizedBox(height: 5),
+                          Text("Projects"),
+                        ],
                       ),
-                    ),
-                  ),
-                  Column(
-                    children: const [
-                      Text("20"),
-                      SizedBox(height: 5),
-                      Text("Students"),
-                    ],
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20),
-                    child: Text(
-                      "|",
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w300,
-                      ),
-                    ),
-                  ),
-                  Column(
-                    children: const [
-                      Text("100"),
-                      SizedBox(height: 5),
-                      Text("Clients"),
-                    ],
-                  ),
-                  SizedBox(width: 10),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 25.0),
-                child: Container(
-                  width: 250,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50),
-                    gradient: LinearGradient(
-                      colors: const [
-                        Color(0xFFFFC09B),
-                        Color(0xFFFF9A82),
-                      ],
-                    ),
-                  ),
-                  child: Center(
-                    child: Text(
-                      'FOLLOW',
-                      style: TextStyle(
-                        fontSize: 17,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.grid_4x4_outlined,
-                    color: Color(0xFFFF9A82),
-                  ),
-                  SizedBox(width: 50),
-                  Icon(
-                    Icons.screen_lock_rotation,
-                    color: Color(0xFFFF9A82),
-                  )
-                ],
-              ),
-              SizedBox(height: 10),
-              Expanded(
-                child: Container(
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Colors.grey[300],
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(25),
-                      topRight: Radius.circular(25),
-                    ),
-                  ),
-                  child: Padding(
-                    padding:
-                        const EdgeInsets.only(top: 8.0, left: 15, right: 15),
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 5.0),
-                          child: Container(
-                            height: 4,
-                            width: 70,
-                            color: Colors.black,
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: Text(
+                          "|",
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.w300,
                           ),
                         ),
-                        Text('Photos'),
-                        SizedBox(height: 12),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Container(
-                                height: 85,
-                                width: 75,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(15),
-                                  image: DecorationImage(
-                                    image: AssetImage("assets/image1.png"),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Container(
-                                height: 85,
-                                width: 75,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(15),
-                                  image: DecorationImage(
-                                    image: AssetImage("assets/image2.png"),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Container(
-                                height: 85,
-                                width: 75,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(15),
-                                  image: DecorationImage(
-                                    image: AssetImage("assets/image3.png"),
-                                  ),
-                                ),
-                              ),
-                            )
+                      ),
+                      Column(
+                        children: const [
+                          Text("20"),
+                          SizedBox(height: 5),
+                          Text("Students"),
+                        ],
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: Text(
+                          "|",
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        children: const [
+                          Text("100"),
+                          SizedBox(height: 5),
+                          Text("Clients"),
+                        ],
+                      ),
+                      SizedBox(width: 10),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 25.0),
+                    child: Container(
+                      width: 250,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        gradient: LinearGradient(
+                          colors: const [
+                            Color(0xFFFFC09B),
+                            Color(0xFFFF9A82),
                           ],
                         ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Container(
-                                height: 85,
-                                width: 75,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(15),
-                                  image: DecorationImage(
-                                    image: AssetImage("assets/image4.png"),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Container(
-                                height: 85,
-                                width: 75,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(15),
-                                  image: DecorationImage(
-                                    image: AssetImage("assets/image5.png"),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Container(
-                                height: 85,
-                                width: 75,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(15),
-                                  image: DecorationImage(
-                                    image: AssetImage("assets/image6.png"),
-                                  ),
-                                ),
-                              ),
-                            )
-                          ],
-                        )
-                      ],
+                      ),
+                      child: Center(
+                        child: Text(
+                          'FOLLOW',
+                          style: TextStyle(
+                            fontSize: 17,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.grid_4x4_outlined,
+                        color: Color(0xFFFF9A82),
+                      ),
+                      SizedBox(width: 50),
+                      Icon(
+                        Icons.screen_lock_rotation,
+                        color: Color(0xFFFF9A82),
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Expanded(
+                    child: Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(25),
+                          topRight: Radius.circular(25),
+                        ),
+                      ),
+                      child: GridView(
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                          crossAxisSpacing: 10,
+                          mainAxisSpacing: 10,
+                        ),
+                        children: List.generate(6, (i) {
+                          return PostImage(
+                            image: 'assets/image${i + 1}.png',
+                          );
+                        }),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PostImage extends StatelessWidget {
+  const PostImage({Key? key, required this.image, this.height, this.width})
+      : super(key: key);
+
+  final String image;
+  final double? height;
+  final double? width;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Container(
+        height: height ?? 85,
+        width: width ?? 75,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          image: DecorationImage(
+            image: AssetImage(image),
           ),
         ),
       ),
